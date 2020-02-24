94b7c9082a77ffb9cdc1f7ad77ace0400946ad0c
Dav <57032623+Dav-Git@users.noreply.github.com>
2020-02-24 14:02:02 +0100

    added helpstrings

--- fkcom/fkcom.py
+++ fkcom/fkcom.py
@@ -7,6 +7,7 @@ class FKCom(commands.Cog):
     @commands.command()
     @checks.mod()
     async def rp(self, ctx, user: Optional[discord.User]):
+        """Blanket warn message for rp questions"""
         await ctx.send(
             "Hey{}, it seems like you have asked a question about ARP (Aspirant Gaming). We are not affiliated with ARP at all and sadly can not provide any information to you. To receive an answer to your question, try {} or the Aspirant Gaming discord server.".format(
                 " " + user.mention if user != None else "\u200b",
@@ -16,6 +17,7 @@ class FKCom(commands.Cog):
 
     @commands.command()
     async def claw(self, ctx, user: discord.Member):
+        f"""``[Member]`` | Puts a member into {ctx.guild.get_channel(483213085293936640).mention}."""
         roles = {
             "fireteam": ctx.guild.get_role(634692203582717990),
             "burning": ctx.guild.get_role(489455280266936321),
@@ -28,7 +30,7 @@ class FKCom(commands.Cog):
                 await user.remove_roles(roles["burning"], reason="Contact claws assigned.")
             await user.add_roles(roles["contact"], reason="Contact claws assigned.")
             await ctx.guild.get_channel(350726339327950859).send(
-                f"{user.name} has been put into {ctx.guild.get_channel(350726339327950859).mention}."
+                f"{user.name} has been put into {ctx.guild.get_channel(483213085293936640).mention}."
             )
         except:
             await ctx.send("An error occured.")

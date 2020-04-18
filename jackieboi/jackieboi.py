4d5c93d7e18ec5b5e5a6a3216d438131c647b7d4
Dav <57032623+Dav-Git@users.noreply.github.com>
2020-04-18 20:16:55 +0200

    add modlog case

--- jackieboi/jackieboi.py
+++ jackieboi/jackieboi.py
@@ -1,10 +1,25 @@
-from redbot.core import commands, checks
+from redbot.core import commands, checks, modlog
 import discord
 from typing import Optional
+from datetime import datetime
 
 
 class JackieBoi(commands.Cog):
     """JackieCog"""
+
+    async def intialize(self, bot):
+        self.bot = bot
+        await self.register_casetypes()
+
+    @staticmethod
+    async def register_casetypes():
+        forcechange_case = {
+            "name": "forcechange",
+            "default_setting": True,
+            "image": ":pencil2:",
+            "case_str": "Nickname force-changed",
+        }
+        await modlog.register_casetype(**forcechange_case)
 
     @checks.mod()
     @commands.command()
@@ -16,6 +31,16 @@ class JackieBoi(commands.Cog):
         else:
             reason = f'"{reason}" requested by {ctx.author.mention}.'
         try:
-            await whoever_the_fuck_needs_changed.edit(nick="change", reason=reason)
+            await whoever_the_fuck_needs_changed.edit(nick="change")
+            await modlog.create_case(
+                self.bot,
+                ctx.guild,
+                datetime.now(),
+                "forcechange",
+                whoever_the_fuck_needs_changed,
+                moderator=ctx.author,
+                reason=reason,
+                channel=ctx.channel,
+            )
         except discord.errors.Forbidden:
             await ctx.send("Missing permissions.")

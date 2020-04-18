e743d560f07e80650dc91910e295822b9cdae582
Dav <57032623+Dav-Git@users.noreply.github.com>
2020-04-18 19:31:15 +0200

    new cog for jackieboi

---
+++ jackieboi/__init__.py
@@ -0,0 +1,5 @@
+from .jackieboi import JackieBoi
+
+
+def setup(bot):
+    bot.add_cog(JackieBoi())
---
+++ jackieboi/jackieboi.py
@@ -0,0 +1,15 @@
+from redbot.core import commands, checks
+import discord
+
+
+class JackieBoi(commands.Cog):
+    """JackieCog"""
+
+    @checks.mod()
+    @commands.command()
+    async def forcenick(
+        self, ctx, whoever_the_fuck_needs_changed: discord.Member, *, reason: Optional[str]
+    ):
+        if not reason:
+            reason = "Nickname force-changed."
+        await whoever_the_fuck_needs_changed.edit(nick="CHANGEME", reason=reason)

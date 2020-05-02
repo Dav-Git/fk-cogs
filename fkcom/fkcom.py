10d1c2352de122387f0f6b1108740db2903dd636
Dav <57032623+Dav-Git@users.noreply.github.com>
2020-05-02 22:55:05 +0000

    Develop (#21)
    * add #no-mic support for FunRoom 2

--- fkcom/fkcom.py
+++ fkcom/fkcom.py
@@ -31,7 +31,12 @@ class FKCom(commands.Cog):
     async def on_voice_state_update(self, member, before, after):
         # Show #no-mic if member in VC
         if after.channel:
-            if after.channel.id in [332834234135740416, 518130862492090415, 481820769568161793]:
+            if after.channel.id in [
+                332834234135740416,
+                518130862492090415,
+                481820769568161793,
+                706267818211147842,
+            ]:
                 overwrite = discord.PermissionOverwrite()
                 overwrite.send_messages = True
                 overwrite.read_messages = True

346a43d6aa9f82c1b48e9610fa046f2a74e7891d
Dav <57032623+Dav-Git@users.noreply.github.com>
2020-05-23 14:50:44 +0200

    add manual mode

--- memcountstatus/memcountstatus.py
+++ memcountstatus/memcountstatus.py
@@ -16,3 +16,14 @@ class MemCountStatus(commands.Cog):
             name=f" over {mc} members.", type=discord.ActivityType.watching
         )
         await self.bot.change_presence(activity=activity)
+
+    @commands.command()
+    async def mcs(self, ctx):
+        await self.bot.wait_until_ready()
+        guild = self.bot.get_guild(332834024831582210)
+        mc = len(guild.members)
+        activity = discord.Activity(
+            name=f" over {mc} members.", type=discord.ActivityType.watching
+        )
+        await self.bot.change_presence(activity=activity)
+        await ctx.send("Manual MCS completed.")

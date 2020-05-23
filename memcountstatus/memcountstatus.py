25346f236926e8857eae3bb67435b3a2bd72122e
Dav <57032623+Dav-Git@users.noreply.github.com>
2020-05-23 15:44:24 +0200

    actually start the task

--- memcountstatus/memcountstatus.py
+++ memcountstatus/memcountstatus.py
@@ -6,6 +6,10 @@ from discord.ext import tasks
 class MemCountStatus(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self._memcount_to_status.start()
+
+    def cog_unload(self):
+        self._memcount_to_status.cancel()
 
     @tasks.loop(minutes=5)
     async def _memcount_to_status(self):
@@ -20,11 +24,4 @@ class MemCountStatus(commands.Cog):
     @checks.admin()
     @commands.command()
     async def mcs(self, ctx):
-        await self.bot.wait_until_ready()
-        guild = self.bot.get_guild(332834024831582210)
-        mc = len(guild.members)
-        activity = discord.Activity(
-            name=f" over {mc} members.", type=discord.ActivityType.watching
-        )
-        await self.bot.change_presence(activity=activity)
-        await ctx.send("Manual MCS completed.")
+        await self._memcount_to_status()

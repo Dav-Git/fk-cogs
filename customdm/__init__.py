0d9184fdc56c2e7191de4174a391bea6e3206098
Dav <57032623+Dav-Git@users.noreply.github.com>
2020-06-05 23:12:58 +0200

    Let's try incoming. Closes #28

--- customdm/__init__.py
+++ customdm/__init__.py
@@ -2,4 +2,4 @@ from .customdm import CustomDM
 
 
 def setup(bot):
-    bot.add_cog(CustomDM())
+    bot.add_cog(CustomDM(bot))
--- customdm/customdm.py
+++ customdm/customdm.py
@@ -3,6 +3,9 @@ from redbot.core import commands, checks
 
 
 class CustomDM(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
     @commands.command(name="message")
     @checks.admin()
     async def send_pm(self, ctx, user: discord.User, *, text: str):
@@ -17,3 +20,28 @@ class CustomDM(commands.Cog):
                     await ctx.guild.get_channel(717915738739965952).send(file=(await e.to_file()))
         except discord.Forbidden:
             await ctx.send("Message couldn't be delivered.")
+
+    @commands.Cog.listener()
+    async def on_message_without_command(self, message):
+        if message.guild is not None:
+            return
+        if message.author == self.bot.user:
+            msg = f"Sent PM to {message.channel.recipient} (`{message.channel.recipient.id}`)"
+            if message.embeds:
+                embed = discord.Embed.from_dict(
+                    {**message.embeds[0].to_dict(), "timestamp": str(message.created_at)}
+                )
+            else:
+                embed = discord.Embed(description=message.content, timestamp=message.created_at)
+            await self.bot.get_guild(332834024831582210).get_channel(717915738739965952).send(
+                msg, embed=embed
+            )
+        else:
+            await self.bot.get_guild(332834024831582210).get_channel(717915738739965952).send(
+                f"Incoming message from {message.author.name}#{message.author.discriminator}:\n{message.content}"
+            )
+            if message.attachments:
+                for e in message.attachments:
+                    await self.bot.get_guild(332834024831582210).get_channel(
+                        717915738739965952
+                    ).send(file=(await e.to_file()))

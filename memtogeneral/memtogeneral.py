2605053c1f618c708101ceab528db2d2b72d9583
Dav <57032623+Dav-Git@users.noreply.github.com>
2020-06-09 21:48:08 +0200

    ooops

--- memtogeneral/memtogeneral.py
+++ memtogeneral/memtogeneral.py
@@ -19,4 +19,6 @@ class MemToGeneral(commands.Cog):
     @commands.command()
     @checks.is_owner()
     async def memtogeneral(self, ctx):
-        await self._memcount_to_general()
+        await self.bot.get_guild(497097726400528394).get_channel(497101356646006784).edit(
+            topic=f"Invite link is: https://discord.gg/zbJtTxe \nCurrently at {len(self.bot.get_guild(497097726400528394).members)} members."
+        )

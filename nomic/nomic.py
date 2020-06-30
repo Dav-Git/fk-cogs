daa21a072e1315d486894e9a49ccfb9ef45f4b8c
Dav <57032623+Dav-Git@users.noreply.github.com>
2020-06-30 22:37:43 +0200

    fix nomic

--- nomic/nomic.py
+++ nomic/nomic.py
@@ -9,23 +9,17 @@ class NoMic(commands.Cog):
     async def on_voice_state_update(self, member, before, after):
         # Show #no-mic if member in VC
         if after.channel:
-            if after.channel.id in [
-                332834234135740416,
-                518130862492090415,
-                481820769568161793,
-                706267818211147842,
-            ]:
-                overwrite = discord.PermissionOverwrite()
-                overwrite.send_messages = True
-                overwrite.read_messages = True
-                await member.guild.get_channel(702969795389292584).set_permissions(
-                    member, overwrite=overwrite
-                )
-            else:
-                await member.guild.get_channel(702969795389292584).set_permissions(
-                    member, overwrite=None
-                )
+            overwrite = discord.PermissionOverwrite()
+            overwrite.send_messages = True
+            overwrite.read_messages = True
+            await member.guild.get_channel(702969795389292584).set_permissions(
+                member, overwrite=overwrite
+            )
         else:
             await member.guild.get_channel(702969795389292584).set_permissions(
                 member, overwrite=None
             )
+    else:
+        await member.guild.get_channel(702969795389292584).set_permissions(
+            member, overwrite=None
+        )

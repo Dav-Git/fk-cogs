faa7c6deea24217e29cc74b57863ca370179cfd5
Dav <57032623+Dav-Git@users.noreply.github.com>
2020-07-02 20:41:30 +0200

    ashley changes

--- ashleyrolejoin/ashleyrolejoin.py
+++ ashleyrolejoin/ashleyrolejoin.py
@@ -10,20 +10,10 @@ class AshleyRoleJoin(commands.Cog):
     async def on_member_join(self, member):
         if member.guild.id == 718316599559585843:
             try:
-                await member.add_roles(
-                    member.guild.get_role(719155457704591361),  # BML
-                    member.guild.get_role(719155463165444147),  # Covid
-                    member.guild.get_role(719155466533470249),  # LEO
-                    member.guild.get_role(719155469591117886),  # Selfies
-                    member.guild.get_role(719155568404725791),  # Food
-                    member.guild.get_role(719155569168220201),  # Trigger warning
-                    reason="New join",
-                )
-            except Exception as e:
-                self.log.exception(e, exc_info=True)
-            try:
                 await member.send(
-                    "Welcome to the ``2020 Mental Health Resource`` discord. \n\nBy default you will be subsribed to all topics in this server. To unsubscribe from one of the topics, visit #self-roles and click twice on the reaction corresponding to the topic you want to unsubscribe from."
+                    "Welcome to the ``Mental Health Together`` discord. \n\nBy default you will not be subsribed to any topic in this server. To subscribe to one of the topics, visit #self-roles and click on the reaction corresponding to the topic you want to unsubscribe from."
                 )
             except:
-                pass
+                self.log.info(
+                    f"{member.name}#{member.discriminator}({member.id}) has their DMs closed."
+                )

cd37cd9362a4ed984b5dfc8afd34804d631b23e6
Dav <57032623+Dav-Git@users.noreply.github.com>
2020-07-03 20:56:25 +0200

    fine tuning

--- truepurge/truepurge.py
+++ truepurge/truepurge.py
@@ -7,8 +7,12 @@ class TruePurge(commands.Cog):
     @checks.admin()
     @commands.command()
     async def truepurge(self, ctx, amount: int):
+        await ctx.send("Starting...", delete_after=30)
         async for message in ctx.channel.history(limit=amount):
             for i in range(5):
-                i
-                await message.delete()
+                try:
+                    await message.delete()
+                except discord.errors.NotFound:
+                    pass
             asyncio.sleep(30)
+        await ctx.send("Done", delete_after=30)

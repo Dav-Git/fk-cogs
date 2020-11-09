67af8d24a3942a4fc4f5e5c80bb35450669d04e8
Dav <57032623+Dav-Git@users.noreply.github.com>
2020-11-10 00:16:12 +0100

    fkcom use ping-safe role

--- fkcom/fkcom.py
+++ fkcom/fkcom.py
@@ -67,7 +67,7 @@ If you would like to request some sort of functionality please describe exactly 
     @commands.command(name="mod")
     async def get_mod_attention(self, ctx, *, details: Optional[str]):
         """Get a moderator to help you."""
-        modrolestr = ctx.guild.get_role(332835206493110272).mention
+        modrolestr = ctx.guild.get_role(775359048342044713).mention
         await ctx.send("A {} has been requested.".format(modrolestr))
         await ctx.guild.get_channel(339741123406725121).send(
             "A {} has been requested in {}.{}".format(
@@ -79,7 +79,7 @@ If you would like to request some sort of functionality please describe exactly 
     @commands.command(name="admin")
     async def get_admin_attention(self, ctx):
         """Get an admin to help you."""
-        adminrolestr = ctx.guild.get_role(332834961407213568).mention
+        adminrolestr = ctx.guild.get_role(775332588196724767).mention
         await ctx.send("An {} has been requested.".format(adminrolestr))
         await ctx.guild.get_channel(360478963115491328).send(
             "An {} has been requested in {}.".format(adminrolestr, ctx.channel.mention),

44d70a3135c36a9f7d590784df6b3f5451ea5f2e
Dav <57032623+Dav-Git@users.noreply.github.com>
2020-11-10 01:05:59 +0100

    be more explicit

--- joinflag/joinflag.py
+++ joinflag/joinflag.py
@@ -1,4 +1,5 @@
 from redbot.core import commands, Config
+from asyncio import sleep
 
 
 class JoinFlag(commands.Cog):
@@ -13,8 +14,9 @@ class JoinFlag(commands.Cog):
     async def on_member_join(self, member):
         if member.guild.id == 332834024831582210:
             if await self.config.member(member).flag():
+                await sleep(1)
                 await member.guild.get_channel(449945421742211082).send(
-                    f"**This member was flagged after leaving!**\n{await self.config.member(member).flag()}"
+                    f"**This member ({member.display_name}) was flagged after leaving!**\n{await self.config.member(member).flag()}"
                 )
                 await self.config.member(member).flag.set(None)
 

3ca517e533044570961e858286dbca377e0433bd
Dav <57032623+Dav-Git@users.noreply.github.com>
2020-12-11 13:51:39 +0100

    customblocklist

---
+++ customblocklist/__init__.py
@@ -0,0 +1,20 @@
+from .customblocklist import CustomBlockList
+
+old_blocklist_add_command = None
+
+
+async def setup(bot):
+    global old_blocklist_add_command
+    group = bot.get_command("blocklist")
+    old_blocklist_add_command = group.get_command("add")
+    group.remove_command("add")
+    cog = CustomBlockList(bot, group)
+    bot.add_cog(cog)
+    await cog.initialize()
+
+
+def teardown(bot):
+    if old_blocklist_add_command is None:
+        return
+    group = bot.get_command("blocklist")
+    group.add_command(old_blocklist_add_command)
---
+++ customblocklist/customblocklist.py
@@ -0,0 +1,55 @@
+from redbot.core import commands, modlog
+import discord
+from datetime import datetime
+
+command_group = None
+
+
+class CustomBlockList(commands.Cog):
+    def __init__(self, bot, group):
+        global command_group
+        command_group = group
+        self.bot = bot
+
+    @staticmethod
+    async def register_casetypes():
+        blocklist_add_case = {
+            "name": "blocklist",
+            "default_setting": True,
+            "image": ":no_pedestrians:",
+            "case_str": "Added to blocklist",
+        }
+        try:
+            await modlog.register_casetype(**blocklist_add_case)
+        except RuntimeError:
+            pass
+
+    async def initialize(self):
+        await self.register_casetypes()
+
+    @command_group.command(name="add", usage="<user>...")
+    async def blocklist_add(self, ctx: commands.Context, *users: discord.Member):
+        """
+        Adds a user to the blocklist.
+        """
+        if not users:
+            await ctx.send_help()
+            return
+
+        for user in users:
+            if isinstance(user, int):
+                user_obj = discord.Object(id=user)
+            else:
+                user_obj = user
+            if await ctx.bot.is_owner(user_obj):
+                await ctx.send(("You cannot add an owner to the blocklist!"))
+                return
+
+        uids = {getattr(user, "id", user) for user in users}
+        await self.bot._whiteblacklist_cache.add_to_blacklist(None, uids)
+        for user in users:
+            await modlog.create_case(
+                self.bot, ctx.guild, datetime.now(), "blocklist", user, ctx.author
+            )
+
+        await ctx.send(("User added to blocklist."))

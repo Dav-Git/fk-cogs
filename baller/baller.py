c90dc3083e26140a95cd499a1ac416c4a565cc46
Dav <dav@mail.stopdavabuse.de>
2021-03-01 20:04:42 +0100

    wtf red

--- baller/baller.py
+++ baller/baller.py
@@ -4,6 +4,6 @@ from redbot.core import commands
 class Baller(commands.Cog):
     @commands.Cog.listener()
     async def on_command(self, ctx):
-        if ctx.command == ctx.bot.get_command("8ball"):
+        if ctx.message.startswith() == ctx.bot.get_command("8"):
             print("hi")
             await ctx.send(f"{ctx.author.mention} asked: {ctx.command.args}")

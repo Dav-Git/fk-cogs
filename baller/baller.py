5e42c500ff329c9fe9560ec4157c9f3095746981
Dav <dav@mail.stopdavabuse.de>
2021-03-01 20:47:12 +0100

    me stupid

--- baller/baller.py
+++ baller/baller.py
@@ -5,6 +5,6 @@ class Baller(commands.Cog):
     @commands.Cog.listener()
     async def on_message(self, message):
         print("boop")
-        if message.startswith("-8"):
+        if message.content.startswith("-8"):
             print("hi")
             await message.channel.send(f"{message.author.mention} asked: {message}")

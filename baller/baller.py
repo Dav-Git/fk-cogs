cd0e682e3c81897df4d0814cfc9048df73a42624
Dav <dav@mail.stopdavabuse.de>
2021-03-01 20:43:08 +0100

    try 10097079ß07

--- baller/baller.py
+++ baller/baller.py
@@ -3,8 +3,8 @@ from redbot.core import commands
 
 class Baller(commands.Cog):
     @commands.Cog.listener()
-    async def on_command_completion(self, ctx):
+    async def on_message(self, message):
         print("boop")
-        if ctx.command == ctx.bot.get_command("8"):
+        if message.startswith("-8"):
             print("hi")
-            await ctx.send(f"{ctx.author.mention} asked: {ctx.command.args}")
+            await message.channel.send(f"{message.author.mention} asked: {message}")

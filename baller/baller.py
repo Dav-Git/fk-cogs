a8ce14b77a0d5fa0126cb0202a456f493cb51abc
Dav <dav@mail.stopdavabuse.de>
2021-03-01 20:26:36 +0100

    hnfgno<yindbgioubn<df

--- baller/baller.py
+++ baller/baller.py
@@ -3,7 +3,7 @@ from redbot.core import commands
 
 class Baller(commands.Cog):
     @commands.Cog.listener()
-    async def on_command(self, ctx):
-        if ctx.message.content.startswith("-8"):
+    async def on_command_completion(self, ctx):
+        if ctx.command == ctx.bot.get_command("8"):
             print("hi")
             await ctx.send(f"{ctx.author.mention} asked: {ctx.command.args}")

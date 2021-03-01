63254d3f7064354bbd7c01cb63ea66fafd342976
Dav <dav@mail.stopdavabuse.de>
2021-03-01 20:03:14 +0100

    hbgi<ugböpo<

--- baller/baller.py
+++ baller/baller.py
@@ -5,4 +5,5 @@ class Baller(commands.Cog):
     @commands.Cog.listener()
     async def on_command(self, ctx):
         if ctx.command == ctx.bot.get_command("8ball"):
+            print("hi")
             await ctx.send(f"{ctx.author.mention} asked: {ctx.command.args}")

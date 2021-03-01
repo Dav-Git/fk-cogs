3f7417f5d40054e896e794259fd23ccb5f14592e
Dav <dav@mail.stopdavabuse.de>
2021-03-01 20:27:44 +0100

    debugging

--- baller/baller.py
+++ baller/baller.py
@@ -4,6 +4,7 @@ from redbot.core import commands
 class Baller(commands.Cog):
     @commands.Cog.listener()
     async def on_command_completion(self, ctx):
+        print("boop")
         if ctx.command == ctx.bot.get_command("8"):
             print("hi")
             await ctx.send(f"{ctx.author.mention} asked: {ctx.command.args}")

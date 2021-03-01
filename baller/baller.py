2b55db7f673f5541d796a2de89963e6d8452e2dd
Dav <dav@mail.stopdavabuse.de>
2021-03-01 20:57:35 +0100

    suck it!

--- baller/baller.py
+++ baller/baller.py
@@ -6,5 +6,5 @@ class Baller(commands.Cog):
     async def on_message(self, message):
         if message.content.startswith("-8"):
             await message.channel.send(
-                f'{message.author.mention} asked: {message.content.replace("-8","",1).replace("-8ball","",1)}'
+                f'{message.author.mention} asked: {message.content.replace("-8ball","",1).replace("-8","",1)}'
             )

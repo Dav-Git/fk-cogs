4a183e4b450e22daad15b60418347fa9963825f5
Dav <dav@mail.stopdavabuse.de>
2021-03-01 20:56:45 +0100

    no backslashes. got it

--- baller/baller.py
+++ baller/baller.py
@@ -5,4 +5,6 @@ class Baller(commands.Cog):
     @commands.Cog.listener()
     async def on_message(self, message):
         if message.content.startswith("-8"):
-            await message.channel.send(f'{message.author.mention} asked: {message.content.replace("-8","\u200b",1).replace("-8ball","\u200b",1)}')
+            await message.channel.send(
+                f'{message.author.mention} asked: {message.content.replace("-8","",1).replace("-8ball","",1)}'
+            )

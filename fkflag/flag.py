46767b448e26c0b80a4f9d9e76f2ed0be339b38d
Dav <dav@mail.stopdavabuse.de>
2021-05-21 11:49:40 +0200

    no aboos

--- fkflag/flag.py
+++ fkflag/flag.py
@@ -81,6 +81,9 @@ class Flag(Cog):
     @commands.command()
     async def delflag(self, ctx, member: discord.Member, *, text: str):
         """Deletes a flag. \n`text` needs to be the FULL flag text."""
+        if ctx.author.id == member.id:
+            await ctx.send("Haha. **No!**")
+            return
         async with self.config.guild(ctx.guild).flags() as allflags:
             flags = allflags[str(member.id)]
             i = 0

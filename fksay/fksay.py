6ddc782089ff3ccb6c2fc601724accff4dc3551b
Dav <dav@mail.stopdavabuse.de>
2021-07-05 11:46:26 +0200

    smh... forgot to await

--- fksay/fksay.py
+++ fksay/fksay.py
@@ -9,7 +9,7 @@ class FkSay(commands.Cog):
         files = []
         if ctx.message.attachments:
             for attachment in ctx.message.attachments:
-                files.append(attachment.to_file())
+                files.append(await attachment.to_file())
         await channel.send(message, files=files, allowed_mentions=discord.AllowedMentions.all())
         await ctx.send(f"{ctx.author.mention} ({ctx.author.display_name}) made me say:")
         await ctx.send(message, files=files, allowed_mentions=discord.AllowedMentions.all())

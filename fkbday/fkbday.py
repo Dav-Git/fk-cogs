30134d66a9bb7211fc5b6d47f3ce21a068dc1c8b
Dav <dav@mail.stopdavabuse.de>
2021-07-22 23:11:55 +0200

    aaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- fkbday/fkbday.py
+++ fkbday/fkbday.py
@@ -1,6 +1,9 @@
-from redbot.core import commands
-import discord
 import asyncio
+import time
+
+import discord
+from discord.ext import tasks
+from redbot.core import Config, commands
 
 
 def isStaff():
@@ -22,20 +25,35 @@ def isStaff():
 
 
 class FKBday(commands.Cog):
+    def __init__(self, bot):
+        self.config = Config.get_conf(self, identifier=959)
+        default_guild = {"bdays": {}}
+        self.config.register_guild(**default_guild)
+        self.bot = bot
+
+    @tasks.loop(hours=1)
+    async def bdayremover(self):
+        todel = []
+        async with self.config.guild_from_id(332834024831582210).bdays as bdays:
+            for date in bdays:
+                if int(time.time()) > date:
+                    guild = self.bot.get_guild(332834024831582210)
+                    await guild.get_member(bdays[date]).remove_roles(
+                        guild.get_role(657943577065947157), reason="Their birthday is over!"
+                    )
+                    todel.append(date)
+        await self.config.guild_from_id(332834024831582210).bdays()
+        for d in todel:
+            del bdays[d]
+        await self.config.guild_from_id(332834024831582210).bdays.set(bdays)
+
     @isStaff()
     @commands.command(aliases=["birthday", "bd"])
     async def bday(self, ctx, *, user: discord.Member):
         """Says happy birthday to a user"""
         await ctx.send("Happy Birthday, **{0}**! :tada:".format(user.display_name))
         await user.add_roles(ctx.guild.get_role(657943577065947157), reason="It's their birthday!")
-        await asyncio.sleep(86400)
-        try:
-            await user.remove_roles(
-                ctx.guild.get_role(657943577065947157), reason="Their birthday is over!"
-            )
-        except:
-            await ctx.guild.get_channel(449944250507984896).send(
-                "Something went wrong when trying to remove the birthday role from {0}".format(
-                    user.display_name
-                )
-            )
+        async with self.config.guild(ctx.guild).bdays() as bdays:
+            # get unix timestamp
+            end_timestamp = int(time.time()) + 86400
+            bdays[end_timestamp] = user.id

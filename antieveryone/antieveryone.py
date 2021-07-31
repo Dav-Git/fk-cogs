3de3f3d3929a5b976d79542569336cd961155462
Dav <57032623+Dav-Git@users.noreply.github.com>
2021-08-01 00:44:22 +0200

    reeeee closes #145

--- antieveryone/antieveryone.py
+++ antieveryone/antieveryone.py
@@ -5,9 +5,10 @@ class AntiEveryone(commands.Cog):
     @commands.Cog.listener()
     async def on_message(self, message):
         if not message.author.bot:
-            if not message.guild.get_role(343116359724433429) in message.author.roles:
-                if "@everyone" in message.content:
-                    await message.delete()
-
             if message.guild.id == 343116121760595971:
-                await message.guild.get_channel(820654163784106034).send(f"``{message.content}``")
+                if not message.guild.get_role(332834961407213568) in message.author.roles:
+                    if "@everyone" in message.content:
+                        await message.delete()
+                        await message.guild.get_channel(449944250507984896).send(
+                            f"{message.author}({message.author.id}) just tried to @everyone. This is the message they attempted to send:\n``{message.content}``"
+                        )

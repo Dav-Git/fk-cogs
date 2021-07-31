5d2195326e693112c55f9e8c47415cc56b8a1fce
Dav <57032623+Dav-Git@users.noreply.github.com>
2021-08-01 00:51:45 +0200

    reee

--- antieveryone/antieveryone.py
+++ antieveryone/antieveryone.py
@@ -10,12 +10,12 @@ class AntiEveryone(commands.Cog):
                     if "@everyone" in message.content:
                         await message.delete()
                         await message.channel.send(
-                            "https://tenor.com/view/discord-ping-everyone-gif-15090883"
+                            "https://tenor.com/view/ping-discord-vr-chat-everyone-gif-16977940"
                         )
                         await message.channel.send(f"Good job {message.author.mention}...")
                         await message.guild.get_channel(449944250507984896).send(
-                            "https://tenor.com/view/discord-ping-everyone-gif-15090883"
+                            "https://tenor.com/view/ping-discord-vr-chat-everyone-gif-16977940"
                         )
                         await message.guild.get_channel(449944250507984896).send(
-                            f"{message.author}({message.author.id}) just tried to @everyone. This is the message they attempted to send:\n``{message.content}``"
+                            f"{message.author}({message.author.id}) just tried to @everyone in {message.channel.mention}. This is the message they attempted to send:\n``{message.content}``"
                         )

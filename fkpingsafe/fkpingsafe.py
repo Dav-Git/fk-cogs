33fd0fe993f4c888423b3f927b10395c96af3601
Dav <57032623+Dav-Git@users.noreply.github.com>
2021-07-31 23:11:24 +0200

    tAKe 2

--- fkpingsafe/fkpingsafe.py
+++ fkpingsafe/fkpingsafe.py
@@ -9,5 +9,9 @@ class FkPingSafe(commands.Cog):
         PINGSAFE_ROLE = ctx.guild.get_role(775359048342044713)
         if PINGSAFE_ROLE in ctx.author.roles:
             await ctx.author.remove_roles(PINGSAFE_ROLE)
+            await ctx.tick()
+            await ctx.send(f"{ctx.author.display_name} you had the Ping Safe role removed.")
         else:
             await ctx.author.add_roles(PINGSAFE_ROLE)
+            await ctx.tick()
+            await ctx.send(f"{ctx.author.display_name} you had the Ping Safe role added.")

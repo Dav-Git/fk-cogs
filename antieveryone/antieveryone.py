84d8f75ac9582e307356c6d3489a9cf56aa37417
Dav <57032623+Dav-Git@users.noreply.github.com>
2021-08-01 00:39:38 +0200

    testo mesto

--- antieveryone/antieveryone.py
+++ antieveryone/antieveryone.py
@@ -5,5 +5,9 @@ class AntiEveryone(commands.Cog):
     @commands.Cog.listener()
     async def on_message(self, message):
         if not message.author.bot:
+            if not message.guild.get_role(343116359724433429) in message.author.roles:
+                if "@everyone" in message.content:
+                    await message.delete()
+
             if message.guild.id == 343116121760595971:
                 await message.guild.get_channel(820654163784106034).send(f"``{message.content}``")

d21b1ae910b62c80c156163e4ab5c34858734e98
Dav <57032623+Dav-Git@users.noreply.github.com>
2021-07-31 22:45:53 +0200

    :tada: fixed

--- avangelroles/avangelroles.py
+++ avangelroles/avangelroles.py
@@ -8,12 +8,9 @@ class AvAngelRoles(commands.Cog):
         await member.add_roles(member.guild.get_role(866475572250148864))
 
     @commands.Cog.listener()
-    async def on_member_change(self, before, after):
+    async def on_member_update(self, before, after):
         before_ids = [r.id for r in before.roles]
         after_ids = [r.id for r in after.roles]
-        await before.guild.get_channel(870041008052772874).send(
-            f"Before {before_ids}\nAfter {after_ids}."
-        )
         if before_ids != after_ids:
             if 866476884253605888 in after_ids or 866491067045511228 in after_ids:
                 if 866475572250148864 in before_ids:

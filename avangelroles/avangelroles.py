6b5fdfb027f5d2a5738ffb8fa0975f90fad3cee3
Dav <57032623+Dav-Git@users.noreply.github.com>
2021-07-31 22:42:00 +0200

    troubleshooting

--- avangelroles/avangelroles.py
+++ avangelroles/avangelroles.py
@@ -11,6 +11,9 @@ class AvAngelRoles(commands.Cog):
     async def on_member_change(self, before, after):
         before_ids = [r.id for r in before.roles]
         after_ids = [r.id for r in after.roles]
+        await before.guild.get_channel(870041008052772874).send(
+            f"Before {before_ids}\nAfter {after_ids}."
+        )
         if before_ids != after_ids:
             if 866476884253605888 in after_ids or 866491067045511228 in after_ids:
                 if 866475572250148864 in before_ids:

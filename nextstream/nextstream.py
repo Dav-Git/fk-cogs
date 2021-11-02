3d491f563a22da82a3d6ebc140f472fb1c269a4c
Dav <dav@mail.stopdavabuse.de>
2021-11-02 15:04:14 +0100

    fix kitty

--- nextstream/nextstream.py
+++ nextstream/nextstream.py
@@ -6,6 +6,7 @@ class NextStream(commands.Cog):
     """NextStream"""
 
     def __init__(self):
+        self.skip = 0
         pass
 
     @commands.command()
@@ -13,15 +14,24 @@ class NextStream(commands.Cog):
         """NextStream"""
         streamTime = int(self._next_stream().timestamp())
         await ctx.send(
-            f"The next :YT_Kitten: Livestream will be on <t:{streamTime}:F>.\n:alarm_clock: That's in <t:{streamTime}:R>."
+            f"The next <:YT_Kitten:545557741947584515> Livestream will be on <t:{streamTime}:F>.\n:alarm_clock: That's in <t:{streamTime}:R>."
         )
 
     def _next_stream(self):
         now = datetime.now()
         oneDay = timedelta(days=1)
+        if self.skip > 0:
+            now = now + timedelta(weeks=self.skip)
         while True:
             now += oneDay
-            dow = now.strftime("%w")
-            if dow == "4":
+            if now.strftime("%w") == "4":
                 now = now.replace(hour=21, minute=0)
                 return now
+
+    @commands.mod()
+    async def skipstream(self, ctx, weeks: int = 0):
+        """Skip the next x Streams"""
+        self.skip = weeks
+        await ctx.send(
+            f"Skipping the next {weeks} Streams. This needs to be reset in case of a bot restart."
+        )

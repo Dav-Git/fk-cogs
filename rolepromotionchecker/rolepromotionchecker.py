ea628a293d1d428d417a6aee73735107152ad9bc
Dav <dav@mail.stopdavabuse.de>
2021-11-19 14:21:26 +0100

    don't need dis

--- rolepromotionchecker/rolepromotionchecker.py
+++ rolepromotionchecker/rolepromotionchecker.py
@@ -9,7 +9,7 @@ class RolePromotionChecker(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.config = Config.get_conf(self, identifier=171120211734)
-        default_role = {"assign_roles": [], "exclude_roles": [], "guild": None}
+        default_role = {"assign_roles": [], "exclude_roles": []}
         self.config.register_role(**default_role)
 
     @commands.Cog.listener()
@@ -41,7 +41,6 @@ class RolePromotionChecker(commands.Cog):
     async def addrole(self, ctx, scan_role: discord.Role, *, assign_roles: List[discord.Role]):
         """Add a role to the list of roles that are scanned regularly and define which role should be assigned when the scanrole is found on a user."""
         await self.config.role(scan_role).assign_roles.set(assign_roles)
-        await self.config.role(scan_role).guild.set(ctx.guild.id)
         await ctx.send(
             f"Added {scan_role.name} to the list of roles that are scanned regularly.\nThe roles {[r.mention for r in assign_roles]} will be assigned."
         )
@@ -53,7 +52,6 @@ class RolePromotionChecker(commands.Cog):
         """Add a role to the list of roles that exclude a user from the scan.
         Leave Empty, to remove all exclusions from the scanrole."""
         await self.config.role(scan_role).exclude_roles.set(exclude_roles)
-        await self.config.role(scan_role).guild.set(ctx.guild.id)
         await ctx.send(
             f"Members with the roles {[r.mention for r in exclude_roles]} will be excluded when scanning {scan_role.mention}"
         )

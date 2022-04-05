c50e48054f915b06e1649a1c1d225123a7a1f101
Dav <57032623+Dav-Git@users.noreply.github.com>
2022-04-05 09:26:18 +0000

    We don't have CR anymore
    Closes #176

--- fkcom/fkcom.py
+++ fkcom/fkcom.py
@@ -72,9 +72,7 @@ class FKCom(commands.Cog):
         """Get a moderator to help you."""
         modrolestr = ctx.guild.get_role(775359048342044713).mention
         await ctx.send(
-            "A {} or {} has been requested.".format(
-                ctx.guild.get_role(530016413616963591).mention,
-                # haha 69 big funny sav
+            "A {} has been requested.".format(
                 ctx.guild.get_role(332835206493110272).mention,
             )
         )

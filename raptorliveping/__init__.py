830c3411c6c138e7e2ff500655239e05d2791e9f
Dav <mail@david-bauch.de>
2022-04-21 21:46:51 +0200

    ding dong

---
+++ raptorliveping/__init__.py
@@ -0,0 +1,5 @@
+from .raptorliveping import RaptorLivePing
+
+
+def setup(bot):
+    bot.add_cog(RaptorLivePing())
---
+++ raptorliveping/raptorliveping.py
@@ -0,0 +1,15 @@
+import discord
+from redbot.core import commands
+
+
+class RaptorLivePing(commands.Cog):
+    """Ping when Raptor goes live"""
+
+    @commands.Cog.listener()
+    async def on_message_without_command(self, message):
+        """Ping when Raptor goes live"""
+        if message.channel.id == 750713006098481193:
+            await message.channel.send(
+                message.guild.get_role(966353849969410169).mention,
+                allowed_mentions=discord.AllowedMentions(roles=True),
+            )

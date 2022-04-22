be318e623386446653ade7c62e2f414e39db1e34
Dav <mail@david-bauch.de>
2022-04-22 02:16:19 +0200

    oopsie

--- raptorliveping/raptorliveping.py
+++ raptorliveping/raptorliveping.py
@@ -9,7 +9,8 @@ class RaptorLivePing(commands.Cog):
     async def on_message_without_command(self, message):
         """Ping when Raptor goes live"""
         if message.channel.id == 750713006098481193:
-            await message.channel.send(
-                message.guild.get_role(966353849969410169).mention,
-                allowed_mentions=discord.AllowedMentions(roles=True),
-            )
+            if not message.author.id == 799201223655292928:
+                await message.channel.send(
+                    message.guild.get_role(966353849969410169).mention,
+                    allowed_mentions=discord.AllowedMentions(roles=True),
+                )

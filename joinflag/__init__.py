e1973ada0fbd2b49a58f1686420fc4d8a174fdbe
Dav-Git <mail@david-bauch.de>
2023-05-05 16:20:56 +0200

    move joinflag to main repo

--- joinflag/__init__.py
+++
@@ -1,7 +0,0 @@
-from .joinflag import JoinFlag
-
-__red_end_user_data_statement__ = "This cog does stores moderation notes attached to a user ID. The notes will be displayed when a user joins the server."
-
-
-def setup(bot):
-    bot.add_cog(JoinFlag())

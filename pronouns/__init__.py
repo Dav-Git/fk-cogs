f1b12d9c5b830d2813c2179c296296ee8e1599e1
Dav <mail@david-bauch.de>
2023-06-20 15:37:30 +0200

    thanks Discord! closes #196

--- pronouns/__init__.py
+++
@@ -1,7 +0,0 @@
-from .pronouns import Pronouns
-
-__red_end_user_data_statement__ = "This cog does not store end user data. All data is stored within discord itself and can be removed by clearing your pronouns or talking to a member of staff."
-
-
-def setup(bot):
-    bot.add_cog(Pronouns(bot))

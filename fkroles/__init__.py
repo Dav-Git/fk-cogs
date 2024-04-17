9327b5ec57209ade36119d3b35ffa8126729b384
Dav-Git <mail@david-bauch.de>
2024-04-17 19:53:46 +0200

    I always test in production 🥺👉👈

--- fkroles/__init__.py
+++ fkroles/__init__.py
@@ -3,6 +3,6 @@ from .fkroles import FKRoles
 __red_end_user_data_statement__ = "This cog does not store end user data."
 
 async def setup(bot):
-    cog = FkRoles(bot)
+    cog = FKRoles(bot)
     await cog.initialize()
     bot.add_cog(cog)
